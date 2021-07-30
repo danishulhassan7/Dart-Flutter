24870a302fdf28d405fcbd79d5407da7b2218c1e
danishulhassan7 <danishulhassan7@gmail.com>
2021-07-30 12:23:29 +0500

    Dart >> Comments in Dart

---
+++ dart-practicecode/Udemy/Dart-Basics/section-2/comments/bin/comments.dart
@@ -0,0 +1,17 @@
+// There are three types of comments in Dart (single line comment)
+
+
+void main(List<String> arguments) {
+
+  print('Hello world! xpf');
+  /*
+  This is just a
+  sample text and
+  we are just checking
+  the dart comments
+  (Multi-line Comment)
+  */
+}
+
+/// Documentation (We use three forward slashes comment in the documentation of Dart
+
