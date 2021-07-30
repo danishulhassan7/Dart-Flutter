26f90a293c6c89e4f9b3a41e6d549cd654f25dd5
danishulhassan7 <danishulhassan7@gmail.com>
2021-07-30 20:34:30 +0500

    Dart @IntelliJ >> IO example (not working)

--- dart-practicecode/Udemy/Dart-Basics/section-2/userinput/bin/userinput.dart
+++ dart-practicecode/Udemy/Dart-Basics/section-2/userinput/bin/userinput.dart
@@ -1,9 +1,21 @@
+// import 'dart:io';
+// import 'dart:async';
+//
+// void main(List<String> arguments) {
+//
+//   stdout.write("What is your name?");
+//   String name = stdin.readLineSync();
+//   stdout.write("Hello ${name}");
+// }
+
 import 'dart:io';
 import 'dart:async';
 
-void main(List<String> arguments) {
+main(List<String> arguments) {
 
-  stdout.write("What is your name? \r\n");
+  stdout.write('What is your name?\r\n');
   String name = stdin.readLineSync();
-  stdout.write("Hello ${name}");
+
+  name.isEmpty ? stderr.write('Name is empty') : stdout.write('Hello ${name} :) \r\n');
+
 }
