786f04c47df0db31a81c1f7016202711feaed004
danishulhassan7 <danishulhassan7@gmail.com>
2021-07-30 21:26:57 +0500

    Dart @VSCode >> Collections: Enum

---
+++ dart-practicecode/Udemy/Dart-Basics/section-3/enum/enum.dart
@@ -0,0 +1,8 @@
+
+enum colors {red, yellow, orange, blue, green, black, purple, white}
+void main() {
+ //we can declare the enum in the scope of main function
+  print(colors);
+  print(colors.values);
+  print(colors.green);
+}
