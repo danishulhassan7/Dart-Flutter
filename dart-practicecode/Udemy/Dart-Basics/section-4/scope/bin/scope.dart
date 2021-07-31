21b7c5f6424528cb5d550ada2b0a163c83c21ae8
danishulhassan7 <danishulhassan7@gmail.com>
2021-07-31 19:11:06 +0500

    Dart @IntelliJ >> Flow Control: Scope

---
+++ dart-practicecode/Udemy/Dart-Basics/section-4/scope/bin/scope.dart
@@ -0,0 +1,12 @@
+
+
+void main(List<String> arguments) {
+  int age  = 25;
+
+  if(age == 23) {
+    print("You are 23 years old");
+  } else {
+    bool hasBills = true;
+    print("You are not ${age} years old and has bills ${hasBills}");
+  }
+}
