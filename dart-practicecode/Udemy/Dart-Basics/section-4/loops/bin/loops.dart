f05264f815435e483d22c2f5e374f87b964ecba8
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-01 19:18:10 +0500

    Dart @IntelliJ >> Flow Control: loops (continue and break keyword)

--- dart-practicecode/Udemy/Dart-Basics/section-4/loops/bin/loops.dart
+++ dart-practicecode/Udemy/Dart-Basics/section-4/loops/bin/loops.dart
@@ -1,5 +1,40 @@
-import 'package:loops/loops.dart' as loops;
+
 
 void main(List<String> arguments) {
-  print('Hello world: ${loops.calculate()}!');
-}
+
+  int value = 6;
+  int max = 5;
+
+
+  do{
+    print(value);
+    value++;
+  } while(value <= max);
+  print("End of do while loop");
+
+
+  int value2 = 1;
+  while(value2 <= max) {
+    print(value2);
+    value2++;
+  }
+  print("End of while loop");
+
+
+// continue and break
+  print("Infinite loop with break and continue keywords");
+  int a = 1;
+  while(true) {
+    print(a);
+    a++;
+
+    if(a == 5) {
+      print("Value is ${a}");
+      continue;
+      }
+    if(a > 10) {
+      print("Value is greater than 10");
+      break;
+    }
+      }
+      }
