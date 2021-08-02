0ade9ae832149b2cfcf8f4550bccd88d7528a03a
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-02 10:45:55 +0500

    Dart @IntelliJ >> Flow Control: loops (Done foreach)

--- dart-practicecode/Udemy/Dart-Basics/section-4/foreach/bin/foreach.dart
+++ dart-practicecode/Udemy/Dart-Basics/section-4/foreach/bin/foreach.dart
@@ -3,8 +3,18 @@
 void main(List<String> arguments) {
 
   var numbers = [1,2,3,4,5,6,7];
-  numbers.forEach((element) {
+  numbers.forEach((int element) {
     print(element);
   });
+
+  // for loop
+
+  List<String> persons = ["Baber", "Virat", "Rizzu"];
+
+  for(int val = 0; val < persons.length; val++) {
+    print("Person at ${val} is : ${persons[val]}");
+  }
+
 }
+
 
