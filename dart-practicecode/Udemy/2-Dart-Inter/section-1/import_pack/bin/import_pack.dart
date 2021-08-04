fa1faed0f6db6aa4f8a8ecca8a4e290173e1324a
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-04 21:43:11 +0500

    Dart-2 @IntelliJ >> Imports: Using http package

--- dart-practicecode/Udemy/2-Dart-Inter/section-1/import_pack/bin/import_pack.dart
+++ dart-practicecode/Udemy/2-Dart-Inter/section-1/import_pack/bin/import_pack.dart
@@ -1,5 +1,15 @@
-import 'package:import_pack/import_pack.dart' as import_pack;
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('Hello world: ${import_pack.calculate()}!');
+
+  //Getting the code of homepage of a site using http package
+
+  var url = "https://danishulhassan7portfolio.web.app/";
+
+  
+  http.get(Uri.parse(url)).then((response){
+    print("Response Status Code: ${response.statusCode}"); //200 means ok // 404 page not found
+    print("Response Status Body: ${response.body}"); //Getting the source of the given url
+  });
+  
 }
