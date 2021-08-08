457ef7e64917121f47312edb8086b7081eb5b264
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-08 22:38:04 +0500

    Flutter-2H >> Scaffold : Container and Text Widget

--- flutter-applications/awesomeapp/lib/main.dart
+++ flutter-applications/awesomeapp/lib/main.dart
@@ -18,11 +18,28 @@ class HomePage extends StatelessWidget {
         child: Container(
           width: 100,
           height: 100,
-          child: Text("A small box"),
+          child: Text(
+            "A small box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+            ),
           padding: const EdgeInsets.all(8),
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            shape: BoxShape.circle,
+            // shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 15,
+                // spreadRadius: 8,
+                offset: Offset(2.0, 5.0),
+              ),
+            ],
+            borderRadius: BorderRadius.circular(12),
             color: Colors.teal[400],
             gradient: LinearGradient(
               colors: [Colors.pink, Colors.purple]
