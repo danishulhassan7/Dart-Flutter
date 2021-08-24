4b752a49196771a863e5c4b7fd86630331e564e9
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-24 10:44:39 +0500

    1-Flutter-Basics >> Section-3: InputWidgets-> Checkboxes & Radios

---
+++ flutter-applications/udemy/flutter-basics/section-3/radiosinput/android/app/src/main/kotlin/com/example/radiosinput/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.radiosinput
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter-applications/udemy/flutter-basics/section-3/radiosinput/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Radios Input in Google Flutter",
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+
+class _MyAppState extends State<MyApp> {
+
+  int _value1 = 0;
+  int _value2 = 0;
+
+  void _setValue1(int? value) => setState(() => _value1 = value!);
+  void _setValue2(int? value) => setState(() => _value2 = value!);
+
+  Widget makeRadios() {
+    List<Widget> list = <Widget> [];
+
+    for(int i = 0; i < 3; i++) {
+      list.add(Radio(value: i, groupValue: _value1, onChanged: _setValue1));
+    }
+
+    Column column = Column (children: [],);
+    return column;
+  }
+
+  Widget makeRadioTiles() {
+    List<Widget> list = <Widget>[];
+
+    for(int i =0; i < 3; i++) {
+      list.add(RadioListTile(value: i, groupValue: _value2, onChanged: _setValue2));
+    }
+
+    Column column = Column (children: [],);
+    return column;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Radios input in Google Flutter"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              makeRadios(),
+              makeRadioTiles()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
