5555970fff4cd54ca347f20dcd0e3f0ce731eaa0
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-25 12:26:21 +0500

    1-Flutter-Basics >> Section-5: Notifications-> BottomModal

--- flutter-applications/udemy/flutter-basics/section-5/bottommodaldialog/lib/main.dart
+++ flutter-applications/udemy/flutter-basics/section-5/bottommodaldialog/lib/main.dart
@@ -19,7 +19,31 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-
+  void _showBottomModal() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(32.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Something went wrong",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+              ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(onPressed: () => Navigator.pop(context), child: Text("Close"))
+            ],
+          ),
+        );
+      }
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +57,10 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             children: <Widget>[
               Text("This is where the Magic happens"),
+              SizedBox(
+                height: 20,
+              ),
+              IconButton(onPressed: _showBottomModal, icon: Icon(Icons.model_training))
             ],
           ),
         ),
