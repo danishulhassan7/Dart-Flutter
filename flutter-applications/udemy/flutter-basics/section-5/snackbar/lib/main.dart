95887b4fb53d71286a40b1acf8f1f4597b647143
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-25 20:17:35 +0500

    1-Flutter-Basics >> Section-5: Notifications-> SnackBar [Done]

--- flutter-applications/udemy/flutter-basics/section-5/snackbar/lib/main.dart
+++ flutter-applications/udemy/flutter-basics/section-5/snackbar/lib/main.dart
@@ -1,52 +1,91 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Notifcations (Dialogs) in Google Flutter",
-      home: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  final GlobalKey<ScaffoldState> _scaffoldstate = new GlobalKey<ScaffoldState>();
-
-  void _showSnackBar() {
-    _scaffoldstate.currentState.showSnackBar(snackbar)(
-      SnackBar(
-        content: Text("Hello World....")
-        )
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Snack Bar"),
-        backgroundColor: Colors.pink,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text("This is where the Magic happens"),
-              ElevatedButton(onPressed: _showSnackBar, child: Text("Show Snack Bar"))
-            ],
-          ),
+  
+  void main() => runApp(MyApp());
+  
+  class MyApp extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Snackbar Notifier in Google Flutter',
+        home: _SnackBarAppExample(),
+        theme: ThemeData(
+          snackBarTheme: SnackBarThemeData(
+            backgroundColor: Colors.teal,
+            actionTextColor: Colors.white,
+            disabledActionTextColor: Colors.grey,
+            contentTextStyle: TextStyle(fontSize: 16),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+            behavior: SnackBarBehavior.floating,
+          )
         ),
-      ),
-    );
+      );
+    }
   }
-}
+  
+  class _SnackBarAppExample extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Snackbar Notifier'),
+          backgroundColor: Colors.green,
+        ),
+        body: _MySnackBar(),
+      );
+    }
+  }
+  
+  class _MySnackBar extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+      final _snackBar1 = SnackBar(
+        content: Text('Simple Snackabr'),
+      );
+  
+      final _snackBar2 = SnackBar(
+        content: Text('Fancy Snackbar'),
+        duration: const Duration(seconds: 10),
+        action: SnackBarAction(
+          label:'Click',
+          onPressed: () {
+            print('Action is clicked');
+          },
+          textColor: Colors.white,
+          disabledTextColor: Colors.grey,
+        ),
+          onVisible: () {
+            print('Snackbar is visible');
+          },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.all(30.0),
+        padding: EdgeInsets.all(15.0),
+      );
+  
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Show SnackBar 1'),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar1);
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              child: Text('Show SnackBar 2'),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar2);
+              },
+            )
+          ],
+        )
+      );
+    }
+  }
