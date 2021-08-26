628612a925c1406fe0089d1541eb42bbd96ccef4
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-26 11:16:08 +0500

    1-Flutter-Basics >> Section-6: Layouts-> Row and Columns

--- flutter-applications/udemy/flutter-basics/section-6/row_column/lib/main.dart
+++ flutter-applications/udemy/flutter-basics/section-6/row_column/lib/main.dart
@@ -8,6 +8,7 @@ class Setup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Layout in Google Flutter",
       theme: ThemeData(
         primarySwatch: Colors.lightBlue
@@ -25,6 +26,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+
+
+  TextEditingController _username = new TextEditingController();
+  TextEditingController _password = new TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +50,33 @@ class _MyAppState extends State<MyApp> {
                 fontSize: 22
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                Text("Username: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                )
+                ,),
+                Expanded(child: TextField(controller: _username,))
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text("Password: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                )
+                ,),
+                Expanded(child: TextField(controller: _password, obscureText: true,))
+              ],
+            ),
+            Padding(
+              padding:EdgeInsets.all(12.0),
+              child: ElevatedButton(onPressed: () => print("Working, $_username"), child: Text("Submit"))
+              ),
             ],
           ),
         ),
