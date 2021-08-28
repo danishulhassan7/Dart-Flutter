73621bd2d26fb06ac6a7b4d0b89c97af48c52faa
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-28 18:08:39 +0500

    1-Flutter-Basics >> Asg: Section-2 -- Btn

--- flutter-applications/udemy/flutter-basics/asg/display_name/lib/main.dart
+++ flutter-applications/udemy/flutter-basics/asg/display_name/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Practice: Google Flutter",
       theme: ThemeData(
         primarySwatch: Colors.lime
@@ -30,7 +31,7 @@ class _AsgState extends State<Asg> {
 
   void _displayName() {
     setState(() {
-      _str = "MY name is Danish Ul Hassan";
+      _str = "My name is Danish Ul Hassan";
     });
   }
 
