e12e493bc0be0dc5db01ea2e66bb30bd0568eb47
danishulhassan7 <danishulhassan7@gmail.com>
2021-08-28 20:25:58 +0500

    1-Flutter-Basics >> Asg: Section-2 -- Snackbar [in progress]

---
+++ flutter-applications/udemy/flutter-basics/asg/snack_bar_name/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Practice: Google Flutter",
+      theme: ThemeData(
+        primarySwatch: Colors.lime
+      ),
+      home: Asg(),
+    );
+  }
+}
+
+class Asg extends StatefulWidget {
+  const Asg({ Key? key }) : super(key: key);
+
+  @override
+  _AsgState createState() => _AsgState();
+}
+
+class _AsgState extends State<Asg> {
+
+  var _str = "";
+  var _display = "";
+
+  void _onChanged(var str) {
+    setState(() {
+      _str = str;
+    });
+  }
+
+  void _displayName() {
+    setState(() {
+      _display = _str;
+    });
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+ //*Snackbar
+    final _snackBar1 = SnackBar(
+      content: Text(_display),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Flutter | @XPF"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+              Text("Name: "),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(child: TextField(
+                onChanged: _onChanged,
+              ),),
+              SizedBox(
+                height: 70,
+              ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Center(
+                    child: ElevatedButton(onPressed: _displayName, child: Text(""))
+                    ),
+                  //   SizedBox(width: 50,),
+                    // Center(child: Text(_display)),
+
+              //*Snackbar
+              ElevatedButton(
+              child: Text('Show Name'),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar1);
+              },
+            ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
