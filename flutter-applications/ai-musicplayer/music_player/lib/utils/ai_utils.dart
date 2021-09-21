f2b7562f4e101a6d61539d6bf1e39100d097d7a2
danishulhassan7 <danishulhassan7@gmail.com>
2021-09-21 10:45:19 +0500

    Dart&Flutter >> AIMusicPlayer : Mixin Utils + dependencies

---
+++ flutter-applications/ai-musicplayer/music_player/lib/utils/ai_utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+
+mixin AiColors {
+  static Color primaryClr1 = Vx.pink500;
+  static Color primaryClr2 = Vx.purple800;
+}
