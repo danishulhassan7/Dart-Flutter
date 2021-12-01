6395be3bf53cdd9f67cb86705d161460d1ca97fe
danishulhassan7 <danishulhassan7@gmail.com>
2021-12-01 22:19:05 +0500

    Flutter >> GetX UI [setup done]

--- flutter-applications/getxbasicui/lib/homepage.dart
+++ flutter-applications/getxbasicui/lib/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,7 +11,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      // backgroundColor: Colors.red,
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 70,
+          left: 30,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "FutureTechWalay",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
