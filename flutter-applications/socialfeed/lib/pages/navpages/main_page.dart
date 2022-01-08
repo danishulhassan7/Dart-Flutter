27641f061fa7adb609e8e63e7ffaca0a1232d9db
danishulhassan7 <danishulhassan7@gmail.com>
2022-01-08 12:21:57 +0500

    Flutter >> CubitSocialFeed [bottomNavBar]

---
+++ flutter-applications/socialfeed/lib/pages/navpages/main_page.dart
@@ -0,0 +1,65 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:socialfeed/pages/navpages/baritem_page.dart';
+import 'package:socialfeed/pages/navpages/home_page.dart';
+import 'package:socialfeed/pages/navpages/my_page.dart';
+import 'package:socialfeed/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+ List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[0],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black.withOpacity(0.8),
+        unselectedItemColor: Colors.grey.withOpacity(0.7),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
+        items:[
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.apps),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Bar"),
+            icon: Icon(Icons.bar_chart_sharp),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("My"),
+            icon: Icon(Icons.person),
+          ),
+        ] 
+      ),
+    );
+  }
+}
