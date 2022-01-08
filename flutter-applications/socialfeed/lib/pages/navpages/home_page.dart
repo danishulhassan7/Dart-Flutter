765157fce82111c50400ee21471e660fb0e6aa69
danishulhassan7 <danishulhassan7@gmail.com>
2022-01-08 14:54:38 +0500

    Flutter >> CubitSocialFeed [homepg -horizontal scroller]

--- flutter-applications/socialfeed/lib/pages/navpages/home_page.dart
+++ flutter-applications/socialfeed/lib/pages/navpages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:socialfeed/misc/colors.dart';
 import 'package:socialfeed/widgets/app_large_text.dart';
+import 'package:socialfeed/widgets/app_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -78,8 +80,11 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               ),
             ),
           ),
+          //TabBarView
           Container(
-            padding: const EdgeInsets.only(left: 20,),
+            padding: const EdgeInsets.only(
+              left: 20,
+            ),
             height: 300,
             width: double.maxFinite,
             child: TabBarView(controller: _tabController, children: [
@@ -105,7 +110,58 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               Text("Waiting..."),
               Text("Walaikum Salam"),
             ]),
-          )
+          ),
+          SizedBox(
+            height: 22,
+          ),
+          //Text
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppLargeText(
+                  text: "Explore More",
+                  size: 21,
+                ),
+                AppText(
+                  text: "See all",
+                  color: AppColors.textColor1,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          //Explore more items
+          Container(
+            height: 110,
+            width: double.maxFinite,
+            margin: const EdgeInsets.only(left: 20),
+            child: ListView.builder(
+                itemCount: 8,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 20, top: 4),
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: DecorationImage(
+                            image: AssetImage("img/mountain.jpeg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+          ),
         ],
       ),
     );
