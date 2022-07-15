96dde72e35439f86f74f9b0180fda8c8569c94b4
danishulhassan7 <danishulhassan7@gmail.com>
2022-07-15 05:43:32 +0500

    u_fluffy >> 2. MyCard [done]

--- u_fluffy/2/mycard/test/widget_test.dart
+++ u_fluffy/2/mycard/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mi_card/main.dart';
 
-import 'package:mycard/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyInfo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
